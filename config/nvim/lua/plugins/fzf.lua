b7ba49a4bc14d0b57ceffc4bf869667fc0460c38
Evan Kirkiles <kirkilese@gmail.com>
2024-11-24 13:26:39 -0500

    Use ctrl+i and ctrl+h for toggling ignored / hidden files in fzf

--- config/nvim/lua/plugins/fzf.lua
+++ config/nvim/lua/plugins/fzf.lua
@@ -1,3 +1,23 @@
 return {
   { "junegunn/fzf", build = "./install --bin" },
+  {
+    "ibhagwan/fzf-lua",
+    opts = function(_, opts)
+      local actions = require("fzf-lua.actions")
+      return vim.tbl_deep_extend("force", opts, {
+        files = {
+          actions = {
+            ["ctrl-i"] = { actions.toggle_ignore },
+            ["ctrl-h"] = { actions.toggle_hidden },
+          },
+        },
+        grep = {
+          actions = {
+            ["ctrl-i"] = { actions.toggle_ignore },
+            ["ctrl-h"] = { actions.toggle_hidden },
+          },
+        },
+      })
+    end,
+  },
 }
